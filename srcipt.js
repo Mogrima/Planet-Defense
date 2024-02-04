c2c6087f7977f2ccf1b5dd99025671bbadde5560
Mogrima <rebel.rs@yandex.ru>
2024-02-04 16:29:21 +0500

    первоначальная настройка проекта

---
+++ srcipt.js
@@ -0,0 +1,8 @@
+"use strict";
+
+window.addEventListener('load', function() {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 800;
+});
