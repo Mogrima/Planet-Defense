6208617b061f8b00ee17bc4a92105f7557234779
Mogrima <rebel.rs@yandex.ru>
2024-02-05 18:52:28 +0500

    подключен класс Game и запущен его метод render, также изменены цвет и ширина обводки

--- srcipt.js
+++ srcipt.js
@@ -1,8 +1,15 @@
 "use strict";
 
+import { Game } from './src/Game.js';
+
 window.addEventListener('load', function() {
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
     canvas.width = 800;
     canvas.height = 800;
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 2;
+
+    const game = new Game(canvas);
+    game.render(ctx);
 });
