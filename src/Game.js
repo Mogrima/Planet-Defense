2c701e283278034263abbdeda6b5ab59d9cf4f4d
Mogrima <rebel.rs@yandex.ru>
2024-02-05 19:47:22 +0500

    создан объект с координатами мыши, координаты записываются в него из прослушивателя событий. Добавлена для разработки отрисовка линии от центра планеты до указателя мыши

--- src/Game.js
+++ src/Game.js
@@ -7,9 +7,23 @@ export class Game {
         this.height = this.canvas.height;
         
         this.planet = new Planet(this);
+
+        this.mouse = {
+            x: 0,
+            y: 0
+        }
+
+        window.addEventListener('mousemove', e => {
+            this.mouse.x = e.offsetX;
+            this.mouse.y = e.offsetY;
+        });
     }
 
     render(context) {
         this.planet.draw(context);
+        context.beginPath();
+        context.moveTo(this.planet.x, this.planet.y);
+        context.lineTo(this.mouse.x, this.mouse.y);
+        context.stroke();
     }
 }
