0ac4f7db2639ac0a959fb245541cb04160067680
Mogrima <rebel.rs@yandex.ru>
2024-02-05 22:39:22 +0500

    создан шаблон класса врага

---
+++ src/Enemy.js
@@ -0,0 +1,43 @@
+export class Enemy {
+    constructor(game) {
+        this.game = game;
+        this.x = 100;
+        this.y = 100;
+        this.radius = 40;
+        this.width = this.radius * 2;
+        this.height = this.radius * 2;
+        this.speedX = 0;
+        this.speedY = 0;
+        this.free = true;
+    }
+
+    start() {
+        this.free = false;
+    }
+
+    reset() {
+        this.free = true;
+    }
+
+    draw(context) {
+        if (!this.free) {
+            context.save();
+            context.beginPath();
+            context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+            context.stroke();
+            context.restore();
+        }
+    }
+
+    update() {
+        if (!this.free) {
+            this.x += this.speedX;
+            this.y += this.speedY;
+
+            if (this.x < 0 || this.x > this.game.width ||
+                this.y < 0 || this.y > this.game.height) {
+                    this.reset();
+                }
+        }
+    }
+}
